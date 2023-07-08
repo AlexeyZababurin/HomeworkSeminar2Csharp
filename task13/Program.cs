d9fb316fe43a7d738460d3a21fcdc20e81fe0a43
Alexey <Lehan347@yandex.ru>
2023-07-08 18:02:22 +0300

    The program was written for task 13

---
+++ task13/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+x = Math.Abs(x); //Возвращает абсолютное значение числа
+if(99 < x & x < 1000)
+{
+    int thirdDigit = x % 10;
+    Console.WriteLine($"Третья цифра числа {x} -> {thirdDigit}");
+}
+else if(x < 100)
+{
+   Console.WriteLine("Третьей цифры нет");
+}
+else if(x > 999)
+{
+    int k = x;
+    while(k >999)
+    {
+        k = k / 10;
+    }
+    int thirdDigit = k % 10;
+    Console.WriteLine($"Третья цифра числа {x} -> {thirdDigit}");
+}
